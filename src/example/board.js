c9b13bb32f041e873774adf02d6ce4ab3ffaebc7
hezhongfeng <287645587@qq.com>
2023-04-17 16:13:21 +0800

    :sparkles: feat: 演化的状态切换

--- src/example/board.js
+++ src/example/board.js
@@ -1,5 +1,7 @@
 import { Application, Graphics } from 'pixi.js';
 
+// const hasBegin = false;
+
 export default refValue => {
   const RowNumber = 70;
 
@@ -8,15 +10,29 @@ export default refValue => {
   let app = new Application({ width: RowNumber * gridLength, height: RowNumber * gridLength });
   refValue.appendChild(app.view);
 
+  const changeState = event => {
+    const square = event.target;
+    const beforeColor = square.beforeColor;
+    square.beforeColor = square.fill.color;
+    square.isLive = !square.isLive;
+    square.clear();
+    square.beginFill(beforeColor);
+    square.drawRect(0, 0, gridLength, gridLength);
+  };
+
   for (let i = 0; i < RowNumber; i++) {
     for (let j = 0; j < RowNumber; j++) {
-      let frame = new Graphics();
+      let square = new Graphics();
       let bgColor = (i + j) % 2 === 0 ? '#87B990' : '#7eb187';
-      frame.beginFill(bgColor);
-      frame.drawRect(0, 0, gridLength, gridLength);
-      frame.position.x = gridLength * i;
-      frame.position.y = gridLength * j;
-      app.stage.addChild(frame);
+      square.beginFill(bgColor);
+      square.drawRect(0, 0, gridLength, gridLength);
+      square.position.x = gridLength * i;
+      square.position.y = gridLength * j;
+      square.interactive = true;
+      square.isLive = false;
+      square.beforeColor = 16777215;
+      square.on('pointerdown', changeState);
+      app.stage.addChild(square);
     }
   }
 };
